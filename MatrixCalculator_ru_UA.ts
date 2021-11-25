995b5fdfc9d9a83806a843f90c2db186afeb6b94
TyPaporotnyk <daniil.svadkovsky@gmail.com>
2021-11-25 19:38:44 +0200

    Created README.md file

--- MatrixCalculator_ru_UA.ts
+++ MatrixCalculator_ru_UA.ts
@@ -5,84 +5,103 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="220"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="27"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="221"/>
         <source>Матрица 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="64"/>
         <location filename="mainwindow.ui" line="133"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="222"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="225"/>
         <source>Построить</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="108"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="224"/>
         <source>Матрица 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="187"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="228"/>
         <source>-&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="180"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="227"/>
         <source>&lt;-</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="91"/>
         <location filename="mainwindow.ui" line="160"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="223"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="226"/>
         <source>Очистить</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="236"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="230"/>
         <source>Операции</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="251"/>
         <location filename="mainwindow.ui" line="255"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="231"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="238"/>
         <source>Сложить</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="260"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="232"/>
         <source>Вычесть</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="265"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="233"/>
         <source>Умножить</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="270"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="234"/>
         <source>Умножить на</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="275"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="235"/>
         <source>Поделить на</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="280"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="236"/>
         <source>Определитель</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="288"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="239"/>
         <source>Расчитать</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="203"/>
+        <location filename="build-MatrixCalculator-Qt_6_2_1_for_macOS-Debug/MatrixCalculator_autogen/include/ui_mainwindow.h" line="229"/>
         <source>Итоговая матрица</source>
         <translation type="unfinished"></translation>
     </message>
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# matrix-calculator
+# Matrix Calculator
+
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](http://choosealicense.com/licenses/mit/)
+![Commits](https://img.shields.io/github/last-commit/TyPaporotnyk/Mandelbrot-set)
+![Version](https://img.shields.io/badge/C++-Solutions-blue.svg?style=flat&logo=c%2B%2B)
+
+
+**matrix calculator** - simple program for working with matrices
+
+## Ui
+
+<img src="https://github.com/TyPaporotnyk/matrix-calculator/blob/main/Docs/img/1.png" width="320" height="200" />
+
+## Install
+
+**To install** you need to either download qt or download from the [link]() </br>
+You need to use CMake version 3.5 or higher
+
+## Task list
+- [x] Sum
+- [x] Substruct
+- [x] Multipli
+- [x] Multiply on
+- [x] Divide on
+- [x] Determinant
+- [ ] Wrapped matrix
+- [ ] matrix equations
+
